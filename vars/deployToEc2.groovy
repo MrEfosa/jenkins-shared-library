1a14b21959706adf1eca9e75c66db80778039d36
Onyekaozuru Tochukwu David <sirdavidchristian@gmail.com>
2026-06-10 10:51:49 +0100

    Remove commented code from deployToEc2.groovy

--- vars/deployToEc2.groovy
+++ vars/deployToEc2.groovy
@@ -3,7 +3,6 @@
 import com.example.Docker
 
 def call(Map config = [:]) {
-    // 1. Instantiate your Docker class, passing 'this' (the Jenkins context)
     def docker = new Docker(this)
     
     docker.deployToEc2(config)
